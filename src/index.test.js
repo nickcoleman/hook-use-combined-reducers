8158f08d45c379fffe22825012ca06b632914092
Nick Coleman <ncoleman@marketaxess.com>
2019-05-27 19:24:51 -0600

    add some eslint and prettier configuration

---
+++ src/index.test.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai'
+import { spy } from 'sinon'
+
+import useCombinedReducers from './'
+
+describe('useCombinedReducer', () => {
+  it('returns a combined state object', () => {
+    const [state] = useCombinedReducers({
+      widget: ['myWidget', () => {}],
+      thing: ['myThing', () => {}],
+    })
+
+    expect(state).to.eql({ widget: 'myWidget', thing: 'myThing' })
+  })
+
+  it('returns a combined dispatch function', () => {
+    const widgetDispatchCallback = spy()
+    const thingDispatchCallback = spy()
+
+    const [, dispatch] = useCombinedReducers({
+      widget: ['widget', widgetDispatchCallback],
+      thing: ['thing', thingDispatchCallback],
+    })
+
+    dispatch({ type: 'AN_ACTION' })
+
+    expect(widgetDispatchCallback.calledOnce).to.eql(true)
+    expect(thingDispatchCallback.calledOnce).to.eql(true)
+  })
+})
