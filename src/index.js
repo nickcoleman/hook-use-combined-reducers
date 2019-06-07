b02bf4201d3ae972e0e2e2da35490f35580d9aa8
Nick Coleman <ncoleman@marketaxess.com>
2019-06-07 09:03:45 -0600

    clean up some naming

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-const useCombinedReducers = combinedReducers => {
-  // Global State
-  const state = Object.keys(combinedReducers).reduce(
-    (acc, key) => ({ ...acc, [key]: combinedReducers[key][0] }),
+const useCombinedReducers = reducersObject => {
+  const state = Object.keys(reducersObject).reduce(
+    (stateObj, key) => ({ ...stateObj, [key]: reducersObject[key][0] }),
     {},
   )
 
-  // Global Dispatch Function
   const dispatch = action =>
-    Object.keys(combinedReducers)
-      .map(key => combinedReducers[key][1])
-      .forEach(fn => fn(action))
+    Object.keys(reducersObject)
+      .map(key => reducersObject[key][1])
+      .forEach(func => func(action))
 
   return [state, dispatch]
 }
